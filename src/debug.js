db45d2884e1ee0b5304db3937aaa4c5726e7dd47
NoboniSultan <sultannoboni@gmail.com>
2025-10-03 23:01:50 -0400

    Final Response & Bonus Question skipped

--- src/debug.js
+++ src/debug.js
@@ -1,24 +1,18 @@
 const coolnessGauge = (numOfFridges) => {
-  return numOfFridges > 5 ? 'You need more fridges.' : 'You are downright chilly!';
+  return numOfFridges >= 4 ? 'You are downright chilly!' : 'You need more fridges.';
 };
 
 const funkoPopAddictionLevel = (numOfFunkoPops) => {
-  if (numOfFunkoPops === 0) {
-    return 'No pops? Maybe try one.';
-  } else if (numOfFunkoPops >= 1) {
-    return 'Only a few? Keep having fun!';
-  } else if (numOfFunkoPops > 10) {
-    return 'You have a problem.';
-  } else if (numOfFunkoPops > 20) {
-    return 'You need help!';
-  } else {
-    return 'You need an intervention!!!';
-  }
+  if (numOfFunkoPops >= 31) return 'You need an intervention!!!';
+  if (numOfFunkoPops > 20) return 'You need help!';
+  if (numOfFunkoPops > 10) return 'You have a problem.';
+  if (numOfFunkoPops >= 1) return 'Only a few? Keep having fun!';
+  return 'No pops? Maybe try one.';
 };
 
-const returnPositiveNegativeZero = (num) => {
-  return num < 0 ? 'Positive' : num === 0 ? 'Zero' : 'Negative';
-};
+const returnPositiveNegativeZero = (num) =>
+  ["Negative", "Zero", "Positive"][Math.sign(num) + 1];
+
 
 module.exports = {
   coolnessGauge,
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,100 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    return "drought"
+  }
+  else if (inches < 2) {
+    return "dry"
+  }
+  else if (inches < 4) {
+    return "average"
+  }
+  else if (inches < 6) {
+    return "rainy"
+  }
+  else if (inches >= 6) {
+    return "flood"
+  }
 };
+console.log(measureRain(0));
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+  if (breed === "snake") {
+    return "Hiss hiss!"
+  } else if (breed === "cat" && age < 5) {
+    return "Mew mew!"
+  } else if (breed === "cat" && age >= 5) {
+    return "Meow meow!"
+  } else if (breed === "dog" && age >= 10) {
+    return "Boof!"
+  } else if (breed === "dog" && age >= 5) {
+    return "Woof woof!"
+  } else if (breed === "dog" && age < 5) {
+    return "Arf arf!"
+  } else {
+    return "Happy birthday!"
+  };
 };
+console.log(happyBirthdayPet("cat", 3));
 
-const funTypes = () => {
+
+const funTypes = (jsType) => {
+  if (typeof jsType === "string") {
+    return "That's just some text.";
+  } else if (typeof jsType === "number") {
+    if (Number.isNaN(jsType)) {
+      return "Well, now you're just showing off.";
+    }
+    return "That's a good number.";
+  } else if (typeof jsType === "boolean") {
+    return "To bool, or not to bool?"; // That is the question...
+  } else if (typeof jsType === "undefined") {
+    return "Nothing, but I didn't set that.";
+  } else if (jsType === null) {
+    return "Nothing, and I did set that.";
+  } else if (Array.isArray(jsType)) {
+    return "I order you to be indexed.";
+  } else if (typeof jsType === "object") {
+    return "Anybody got the key?";
+  } else {
+    return "Unknown type!";
+  }
 };
+console.log(funTypes({}));
 
-const rounder = () => {
+const rounder = (float, roundingSetting) => {
+  if (roundingSetting === "up") {
+    return Math.ceil(float);
+  } else if (roundingSetting === "down") {
+    return Math.floor(float);
+  } else if (roundingSetting === "honest") {
+    return Math.round(float);
+  }
 };
+console.log(rounder(3.7, "down"));
 
-const formatName = () => {
+const formatName = (first, last) =>
+  `${first[0].toUpperCase()}${first.slice(1).toLowerCase()} ${last[0].toUpperCase()}${last.slice(1).toLowerCase()}`;
+
+console.log(formatName("jOhN", "doE"));
+
+const extractDomain = (email) => {
+  return email.split("@")[1];
 };
+console.log(extractDomain('user@example.com'));
 
-const extractDomain = () => {
+const startsWithVowel = (str) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  if (vowels.includes(str[0])) {
+    return true;
+  } else {
+    return false;
+  }
 };
+console.log(startsWithVowel("apple"));
 
-const startsWithVowel = () => {
-};
-
-const rotate = () => {
+const rotate = (str, num) => {
+  const rotation = num % str.length;
+  return str.slice(-rotation) + str.slice(0, -rotation);
 };
 
 module.exports = {
